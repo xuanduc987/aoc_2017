d4695efdcceee1dcc9c62a72d8719e9e87261f0e
Duc Nghiem Xuan <git@ducnx.com>
2017-12-02 23:25:09 +0900

    Day 2 part 2

--- day2.fsx
+++ day2.fsx
@@ -17,5 +17,28 @@ let part1 lines =
         max' - min')
     |> Array.sum
 
-part1 lines
+let findDevide arr =
+    let sorted = Array.sortDescending arr
+    let l = Array.length arr
+    let rec check l (arr: int[]) i =
+        let recur = check l arr
+        let rec loop n l (arr: int[]) j =
+            let recur = loop n l arr
+            if j >= l then None
+            else
+                if n % arr.[j] = 0 then Some(n / arr.[j])
+                else recur (j + 1)
+        match loop arr.[i] l arr (i+1) with
+        | None -> recur (i+1)
+        | Some v -> v
+    check l sorted 0
+
+let part2 lines =
+    lines
+    |> Array.map parseLine
+    |> Array.map findDevide
+    |> Array.sum
+
+part2 lines
 |> printfn "%d"
+
